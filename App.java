131ddeac35960e7a4061bd2f1d86b46f7583f87f
kimsh <tkim112002@gmail.com>
2022-11-17 14:20:08 -0700

    removed MainClass.java

--- App.java
+++ App.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
 public class App extends Application {
@@ -11,10 +13,11 @@ public class App extends Application {
      @Override
      public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource(".fxml/FXMLDocument.fxml"));
-         
-        Scene scene = new Scene(root);
-        //KeyCombination z = ;
-        stage.setFullScreen(true);
+        
+        GridPane grid= new GridPane();
+        StackPane pane = new StackPane(root);
+        Scene scene = new Scene(pane, 800, 500);
+        
         stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         stage.setFullScreenExitHint(STYLESHEET_CASPIAN);
         stage.setScene(scene);
